810c631ad1894b230b7172c978c0254e48e1c709
jo <chjo0330@gmail.com>
2022-05-07 02:04:09 +0900

    평균값 구하기, 큰 놈, 작은 놈, 같은 놈

--- sw_expert_academy.py
+++ sw_expert_academy.py
@@ -1,4 +1,44 @@
 # 1. 홀수만 더하기
+# t = input()
+
+# count = 1
+
+# dic = {}
+
+# for index in range(int(t)):
+#     data = list(map(int, input().split()))
+#     result = 0
+#     for value in range(len(data)):
+#         if(data[value] % 2 != 0):
+#             result += data[value]
+#     dic[index+1] = result
+
+
+# while count <= int(t):
+#     print('#' + str(count) + " " + str(dic[count]))
+#     count += 1
+
+# 2. 평균값 구하기
+# t = input()
+
+# count = 1
+
+# dic = {}
+
+# for index in range(int(t)):
+#     data = list(map(int, input().split()))
+#     result = 0
+#     for value in range(len(data)):
+#         result += data[value]
+#         average = round(result / len(data))
+#     dic[index+1] = average
+
+
+# while count <= int(t):
+#     print('#' + str(count) + " " + str(dic[count]))
+#     count += 1
+
+# 3. 큰 놈, 작은 놈, 같은 놈
 t = input()
 
 count = 1
@@ -7,10 +47,15 @@ dic = {}
 
 for index in range(int(t)):
     data = list(map(int, input().split()))
-    result = 0
-    for value in range(len(data)):
-        if(data[value] % 2 != 0):
-            result += data[value]
+    result = ''
+
+    if data[0] > data[1]:
+        result = '>'
+    elif data[0] < data[1]:
+        result = '<'
+    elif data[0] == data[1]:
+        result = '='
+
     dic[index+1] = result
 
 
