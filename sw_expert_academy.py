e35bf54e274393bf09b48ffe1d811aafd4ba72ab
jo <chjo0330@gmail.com>
2022-05-08 00:33:48 +0900

    큰 놈, 작은 놈, 같은 놈/최대수 구하기/중간값 구하기

--- sw_expert_academy.py
+++ sw_expert_academy.py
@@ -39,26 +39,56 @@
 #     count += 1
 
 # 3. 큰 놈, 작은 놈, 같은 놈
+# t = input()
+
+# count = 1
+
+# dic = {}
+
+# for index in range(int(t)):
+#     data = list(map(int, input().split()))
+#     result = ''
+
+#     if data[0] > data[1]:
+#         result = '>'
+#     elif data[0] < data[1]:
+#         result = '<'
+#     elif data[0] == data[1]:
+#         result = '='
+
+#     dic[index+1] = result
+
+
+# while count <= int(t):
+#     print('#' + str(count) + " " + str(dic[count]))
+#     count += 1
+
+# 4. 최대수 구하기
+# t = input()
+
+# count = 1
+
+# dic = {}
+
+# for index in range(int(t)):
+#     data = list(map(int, input().split()))
+#     result = 0
+
+#     for i in range(len(data)):
+#         result = max(result, data[i])
+
+#     dic[index+1] = result
+
+
+# while count <= int(t):
+#     print('#' + str(count) + " " + str(dic[count]))
+#     count += 1
+
+# 5. 중간값 구하기
 t = input()
 
-count = 1
+data = list(map(int, input().split()))
 
-dic = {}
+data = sorted(data)
 
-for index in range(int(t)):
-    data = list(map(int, input().split()))
-    result = ''
-
-    if data[0] > data[1]:
-        result = '>'
-    elif data[0] < data[1]:
-        result = '<'
-    elif data[0] == data[1]:
-        result = '='
-
-    dic[index+1] = result
-
-
-while count <= int(t):
-    print('#' + str(count) + " " + str(dic[count]))
-    count += 1
+print(data[len(data)//2])
