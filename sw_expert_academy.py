d2bcb4c477ae8d5f404194924deb2d0a5cf7608b
jo <chjo0330@gmail.com>
2022-05-08 18:51:59 +0900

    연월일 달력/약수 출력

--- sw_expert_academy.py
+++ sw_expert_academy.py
@@ -138,40 +138,52 @@
 # print(letter)
 
 # 10. 연월일 달력
-t = input()
+# t = input()
 
-count = 1
+# count = 1
 
-dic = {}
+# dic = {}
 
-for index in range(int(t)):
-    data = input()
+# for index in range(int(t)):
+#     data = input()
 
-    result = ''
+#     result = ''
 
-    year = data[0:4]
-    month = data[4:6]
-    day = data[6:8]
+#     year = data[0:4]
+#     month = data[4:6]
+#     day = data[6:8]
 
-    if int(month) > 12 or int(month) < 1:
-        dic[index+1] = '-1'
-        continue
+#     if int(month) > 12 or int(month) < 1:
+#         dic[index+1] = '-1'
+#         continue
 
-    if int(month) == 1 or int(month) == 3 or int(month) == 5 or int(month) == 7 or int(month) == 8 or int(month) == 10 or int(month) == 12:
-        if int(day) < 1 or int(day) > 31:
-            dic[index+1] = '-1'
-            continue
-    elif int(month) == 4 or int(month) == 6 or int(month) == 9 or int(month) == 11:
-        if int(day) < 1 or int(day) > 30:
-            dic[index+1] = '-1'
-            continue
-    elif int(month) == 2:
-        if int(day) < 1 or int(day) > 28:
-            dic[index+1] = '-1'
-            continue
-    result = year + '/' + month + '/' + day
-    dic[index+1] = result
+#     if int(month) == 1 or int(month) == 3 or int(month) == 5 or int(month) == 7 or int(month) == 8 or int(month) == 10 or int(month) == 12:
+#         if int(day) < 1 or int(day) > 31:
+#             dic[index+1] = '-1'
+#             continue
+#     elif int(month) == 4 or int(month) == 6 or int(month) == 9 or int(month) == 11:
+#         if int(day) < 1 or int(day) > 30:
+#             dic[index+1] = '-1'
+#             continue
+#     elif int(month) == 2:
+#         if int(day) < 1 or int(day) > 28:
+#             dic[index+1] = '-1'
+#             continue
+#     result = year + '/' + month + '/' + day
+#     dic[index+1] = result
 
-while count <= int(t):
-    print('#' + str(count) + " " + str(dic[count]))
-    count += 1
+# while count <= int(t):
+#     print('#' + str(count) + " " + str(dic[count]))
+#     count += 1
+
+# 11. 약수 출력
+n = int(input())
+
+result = ''
+
+for i in range(n):
+    if n % (i+1) == 0:
+        result += str(i+1)
+        result += ' '
+
+print(result)
