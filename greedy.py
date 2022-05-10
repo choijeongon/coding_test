2000720aeaf8577451412189792b489390d24e9d
최정온 <chjo0330@gmail.com>
2022-05-10 12:40:47 +0900

    큰 수의 법칙 복습

--- greedy.py
+++ greedy.py
@@ -182,6 +182,29 @@ print(count)
 
 # print("result: " + str(result))
 
+# 정답 코드
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+
+while True:
+    for i in range(k): # 가장 큰 수를 K번 더하기
+        if m == 0:
+            break
+        result += first
+        m -= 1 # 더할 때마다 1씩 빼기
+    if m == 0:
+        break
+    result += second
+    m -= 1
+
+print(result)
+
 # 4. 숫자 카드 게임
 # n, m = map(int, input().split())
 
