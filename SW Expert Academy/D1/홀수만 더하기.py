15cc417987bf0cb4110fc8d7ab60da2dca3dfa1e
최정온 <chjo0330@gmail.com>
2022-05-19 10:28:05 +0900

    홀수만 더하기 업데이트

--- SW Expert Academy/D1/홀수만 더하기.py
+++ SW Expert Academy/D1/홀수만 더하기.py
@@ -1,18 +1,36 @@
-t = input()
+# t = input()
 
-count = 1
+# count = 1
 
-dic = {}
+# dic = {}
 
-for index in range(int(t)):
+# for index in range(int(t)):
+#     data = list(map(int, input().split()))
+#     result = 0
+#     for value in range(len(data)):
+#         if(data[value] % 2 != 0):
+#             result += data[value]
+#     dic[index+1] = result
+
+
+# while count <= int(t):
+#     print('#' + str(count) + " " + str(dic[count]))
+#     count += 1
+
+T = int(input())
+
+result_map = {}
+
+for i in range(T):
     data = list(map(int, input().split()))
+
     result = 0
-    for value in range(len(data)):
-        if(data[value] % 2 != 0):
-            result += data[value]
-    dic[index+1] = result
 
+    for val in data:
+        if(val % 2 != 0):
+            result += val
+    
+    result_map[i+1] = result
 
-while count <= int(t):
-    print('#' + str(count) + " " + str(dic[count]))
-    count += 1
+for i in range(T):
+    print("#" + str(i+1) + " " + str(result_map[i+1]))
