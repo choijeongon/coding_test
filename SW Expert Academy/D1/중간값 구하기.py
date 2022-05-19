1ba852ad4cb3a89436a40c11ad8eaf3249bc8625
최정온 <chjo0330@gmail.com>
2022-05-19 10:41:36 +0900

    중간값 구하기 업데이트

--- SW Expert Academy/D1/중간값 구하기.py
+++ SW Expert Academy/D1/중간값 구하기.py
@@ -1,7 +1,18 @@
-t = input()
+# t = input()
+
+# data = list(map(int, input().split()))
+
+# data = sorted(data)
+
+# print(data[len(data)//2])
+
+
+N = int(input())
 
 data = list(map(int, input().split()))
 
-data = sorted(data)
+data.sort()
 
 print(data[len(data)//2])
+
+# sort는 자신을 직접 바꾸고, sorted는 정렬한 배열을 새로 리턴한다.
