1db462d8e5dd39342a990dac25b6d177545cb77a
최정온 <chjo0330@gmail.com>
2022-05-19 10:22:11 +0900

    .

---
+++ SW Expert Academy/D2/새로운 불면증 치료법.py
@@ -0,0 +1,18 @@
+
+# from itertools import count
+
+
+# T = int(input())
+
+# result_map = {}
+
+# for i in range(T):
+#     N = int(input())
+
+#     li = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#     count = 1
+
+#     while li:    
+#         value = count 
+
+#         count += 1
