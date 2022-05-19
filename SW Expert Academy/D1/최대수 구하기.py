862c0f11a1a14fb0834930e7f57f188820a7f746
최정온 <chjo0330@gmail.com>
2022-05-19 10:21:30 +0900

    최대수 구하기 업데이트

--- SW Expert Academy/D1/최대수 구하기.py
+++ SW Expert Academy/D1/최대수 구하기.py
@@ -1,20 +1,36 @@
 
-t = input()
+# t = input()
 
-count = 1
+# count = 1
 
-dic = {}
+# dic = {}
 
-for index in range(int(t)):
+# for index in range(int(t)):
+#     data = list(map(int, input().split()))
+#     result = 0
+
+#     for i in range(len(data)):
+#         result = max(result, data[i])
+
+#     dic[index+1] = result
+
+
+# while count <= int(t):
+#     print('#' + str(count) + " " + str(dic[count]))
+#     count += 1
+
+T = int(input())
+
+result_map = {}
+
+for i in range(T):
     data = list(map(int, input().split()))
     result = 0
 
-    for i in range(len(data)):
-        result = max(result, data[i])
+    for val in data:
+        result = max(result, val)
+    
+    result_map[i+1] = result
 
-    dic[index+1] = result
-
-
-while count <= int(t):
-    print('#' + str(count) + " " + str(dic[count]))
-    count += 1
+for i in range(T):
+    print("#" + str(i+1) + " " + str(result_map[i+1]))
