543a800bd58c1b95ece222a46b144eb69dd8f116
최정온 <chjo0330@gmail.com>
2022-05-19 11:19:30 +0900

    알파벳을 숫자로 변환 업데이트

--- SW Expert Academy/D1/알파벳을 숫자로 변환.py
+++ SW Expert Academy/D1/알파벳을 숫자로 변환.py
@@ -1,14 +1,24 @@
-t = input()
+# t = input()
 
-letter_list = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-number_li = list(range(1, 27))
+# letter_list = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+# number_li = list(range(1, 27))
 
-string_list = ''
+# string_list = ''
 
-for value in list(t):
-    for check in letter_list:
-        if(check == value):
-            string_list += str(number_li[letter_list.index(check)])
-            string_list += ' '
+# for value in list(t):
+#     for check in letter_list:
+#         if(check == value):
+#             string_list += str(number_li[letter_list.index(check)])
+#             string_list += ' '
 
-print(string_list)
+# print(string_list)
+
+letter = input()
+
+li = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+input_list = list(letter)
+
+for val in input_list:
+    print(li.index(val)+ 1, end=' ')
+
