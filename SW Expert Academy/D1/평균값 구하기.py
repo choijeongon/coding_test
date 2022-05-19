90c0d10c53dda70af4b4eb6fd45dd53f5e753b7e
최정온 <chjo0330@gmail.com>
2022-05-19 10:35:11 +0900

    평균값 구하기 업데이트

--- SW Expert Academy/D1/평균값 구하기.py
+++ SW Expert Academy/D1/평균값 구하기.py
@@ -1,18 +1,40 @@
-t = input()
+# t = input()
 
-count = 1
+# count = 1
 
-dic = {}
+# dic = {}
 
-for index in range(int(t)):
+# for index in range(int(t)):
+#     data = list(map(int, input().split()))
+#     result = 0
+#     for value in range(len(data)):
+#         result += data[value]
+#         average = round(result / len(data))
+#     dic[index+1] = average
+
+
+# while count <= int(t):
+#     print('#' + str(count) + " " + str(dic[count]))
+#     count += 1
+
+
+
+T = int(input())
+
+result_map = {}
+
+for i in range(T):
     data = list(map(int, input().split()))
     result = 0
-    for value in range(len(data)):
-        result += data[value]
-        average = round(result / len(data))
-    dic[index+1] = average
 
+    for val in data:
+        result += val
 
-while count <= int(t):
-    print('#' + str(count) + " " + str(dic[count]))
-    count += 1
+    average = round(result / len(data))
+
+    result_map[i+1] = average
+
+for i in range(T):
+    print("#" + str(i+1) + " " + str(result_map[i+1]))
+    
+     
