cff97667d592cbe7f834ab20ee552be1f2825dd7
최정온 <chjo0330@gmail.com>
2022-05-20 11:00:53 +0900

    달팽이 숫자 문제 완성

--- SW Expert Academy/D2/달팽이 숫자.py
+++ SW Expert Academy/D2/달팽이 숫자.py
@@ -1,36 +1,43 @@
 
-# T = int(input())
+T = int(input())
 
-# # row, col 인덱스로 탐색할 수 있게 방향 설정 (달팽이 방향이니까 우->하->좌->상)
-# dr = [0, 1, 0, -1]
-# dc = [1, 0, -1, 0]
+result_map = {}
 
-# for tc in range(1, T+1):
-#     N = int(input())
-#     snail = [[0]*N for _ in range(N)]
-#     # 초기 위치 & 회전방향 설정
-#     r, c = 0, 0
-#     dist = 0  # 0:우, 1:하, 2:좌, 3:상
+for index in range(T):
+    N = int(input())
+    rows = N
+    cols = N
+    board = [[0 for col in range(cols)] for row in range(rows)]
 
-#     for n in range(1, N*N + 1):
-#         snail[r][c] = n
-#         r += dr[dist]
-#         c += dc[dist]
+    # 초기 위치 & 회전방향 설정
+    row, column = 0, 0
+    # 0:우, 1:하, 2:좌, 3:상
+    direction = 0
 
-#         # 범위를 벗어나거나 0이 아닌 다른 값이 이미 있다면, dist 방향 체인지
-#         # 근데 이런 경우라면 요소 인덱스를 다시 원위치시켜줘야하므로 빼줘야함
-#         # 그런다음 dist의 방향을 바꾸고, 방향 바꿨으면 다시 움직일 수 있도록 행렬 인덱스 업데이트
-#         if r < 0 or c < 0 or r >= N or c >= N or snail[r][c] != 0:
-#             # 실행취소
-#             r -= dr[dist]
-#             c -= dc[dist]
-#             # dist는 % 4 안해주면 0123, 4567, ... 이런식으로 숫자 커지므로 나머지로 접근
-#             dist = (dist + 1) % 4
-#             #  다시 gogo
-#             r += dr[dist]
-#             c += dc[dist]
+    # row, col 인덱스로 탐색할 수 있게 방향 설정 
+    # (달팽이 방향이니까 우->하->좌->상)
+    direction_row = [0, 1, 0, -1]
+    direction_column = [1, 0, -1, 0]
 
-#     print("#{}".format(tc))
-#     for row in snail:
-#         print(*row)
-#     print()
+    for value in range(1, N*N +1):
+        board[row][column] = value
+        row += direction_row[direction]
+        column += direction_column[direction]
+
+        if row < 0 or column < 0 or row >= N or column >= N or board[row][column] != 0:
+            # 실행취소
+            row -= direction_row[direction]
+            column -= direction_column[direction]
+
+            direction = (direction + 1) % 4
+
+            row += direction_row[direction]
+            column += direction_column[direction]
+
+    result_map[N] = board
+
+for index in range(T):
+    print("#" + str(index+1))
+    
+    for value in result_map[index+1]:
+        print(*value)
