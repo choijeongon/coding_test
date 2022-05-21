19e15d058f2c50d00c564055c0cd7e05eb93a2e3
최정온 <chjo0330@gmail.com>
2022-05-21 15:15:26 +0900

    백만 장자 프로젝트

---
+++ SW Expert Academy/D2/백만 장자 프로젝트.py
@@ -0,0 +1,20 @@
+
+T = int(input())
+
+for testcase_index in range(1, T+1):
+    N = int(input())
+    price_list = list(map(int, input().split()))
+
+    max_value = price_list[-1]
+
+    profit =0
+
+    for index in range(N-2, -1, -1):
+        #만약 현재 매매가가 최대값보다 크면 최대값 변경
+        if price_list[index] >= max_value:
+            max_value = price_list[index]
+        # 현재 매매가가 최대값보다 크지 않으면 차익을 profit 변수에 더해준다.
+        else:
+            profit += max_value - price_list[index]
+    
+    print('#{} {}'.format(testcase_index, profit))
