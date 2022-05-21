089516214711a64d813e609b9aaae5862ec6312d
최정온 <chjo0330@gmail.com>
2022-05-21 16:46:02 +0900

    상하좌우 다시 풀어보기

--- 이취코테/구현/상하좌우.py
+++ 이취코테/구현/상하좌우.py
@@ -1,21 +1,46 @@
-# 1. 상하좌우
-n = int(input())
-x, y = 1, 1
-plans = input().split()
+# # 1. 상하좌우
+# n = int(input())
+# x, y = 1, 1
+# plans = input().split()
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-move_types = ['L', 'R', 'U', 'D']
+# dx = [0, 0, -1, 1]
+# dy = [-1, 1, 0, 0]
+# move_types = ['L', 'R', 'U', 'D']
 
-# 이동 계획을 하나씩 확인
-for plan in plans:
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx < 1 or ny < 1 or nx > n or ny > n:
-        continue
-    x, y = nx, ny
+# # 이동 계획을 하나씩 확인
+# for plan in plans:
+#     for i in range(len(move_types)):
+#         if plan == move_types[i]:
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#     if nx < 1 or ny < 1 or nx > n or ny > n:
+#         continue
+#     x, y = nx, ny
 
-print(x, y)
+# print(x, y)
     
+
+N = int(input())
+
+li = list(input().split())
+
+# 왼, 오, 위, 아
+move_col = [0, 0, -1, 1]
+move_row = [-1, 1, 0, 0]
+
+action = ['L', 'R', 'U', 'D']
+
+current_col = 1
+current_row = 1
+
+for value in li:
+    for index in range(len(action)):
+        if value == action[index]:
+            current_col += move_col[index]
+            current_row += move_row[index]
+
+            if current_col < 1 or current_row < 1 or current_col > N or current_row > N:
+                current_col -= move_col[index]
+                current_row -= move_row[index]
+
+print('{} {}'.format(current_col, current_row))
