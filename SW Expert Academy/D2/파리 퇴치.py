c720682d07d29be0779341e861da4c3082e85ab2
jo <chjo0330@gmail.com>
2022-05-22 19:19:46 +0900

    파리 퇴치 민수형 풀이

--- SW Expert Academy/D2/파리 퇴치.py
+++ SW Expert Academy/D2/파리 퇴치.py
@@ -29,3 +29,21 @@ for index in range(1, T+1):
 for index in range(1, T+1):
     print("#" + str(index) +" " +str(result_map[index]))
 
+
+# 민수형 풀이
+# def kill_fly(y:int, x:int, M:int)->int:
+#     sum = 0
+#     for i in range(y, y+M):
+#         for j in range(x, x+M):
+#             sum += area[i][j]
+#     return sum
+
+# T = int(input())
+# for test_case in range(1, T+1):
+#     N, M = map(int, input().split())
+#     area = [list(map(int, input().split())) for _ in range(N)]
+#     store = []
+#     for i in range(abs(M-N)+1):
+#         for j in range(abs(M-N)+1):
+#             store.append(kill_fly(i, j, M))
+#     print(f'#{test_case} {max(store)}')
