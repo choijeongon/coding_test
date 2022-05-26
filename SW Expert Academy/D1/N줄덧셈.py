fb7ef64f1eb9aa6fc1978f44924a8cdafbffa207
jo <chjo0330@gmail.com>
2022-05-26 16:05:07 +0900

    .

--- SW Expert Academy/D1/N줄덧셈.py
+++ SW Expert Academy/D1/N줄덧셈.py
@@ -1,8 +1,19 @@
-n = int(input())
+#1
+# n = int(input())
 
-count = 0
+# count = 0
 
-for index in range(1, n+1):
-    count += index
+# for index in range(1, n+1):
+#     count += index
 
-print(count)
+# print(count)
+
+#2
+input_value = int(input())
+
+add_all = 0
+
+for index in range(1, input_value+1):
+    add_all += index
+
+print(add_all)
