2140ee69ea72e8ca5bcfec8a4a4784c3dcbb26fd
jo <chjo0330@gmail.com>
2022-05-26 16:02:50 +0900

    서랍의 비밀번호

--- SW Expert Academy/D1/서랍의 비밀번호.py
+++ SW Expert Academy/D1/서랍의 비밀번호.py
@@ -1,11 +1,25 @@
-p, k = list(map(int, input().split()))
 
-count = 1
+#1
+# p, k = list(map(int, input().split()))
 
-for i in range(p):
-    if k == p:
-        break
+# count = 1
+
+# for i in range(p):
+#     if k == p:
+#         break
+#     count += 1
+#     k += 1
+
+# print(count)
+
+
+#2
+P, K = map(int, input().split())
+
+count = 0
+
+while P >= K:
+    K += 1
     count += 1
-    k += 1
 
 print(count)
