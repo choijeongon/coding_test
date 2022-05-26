635d771f1397e4e0f19fc58fb2bf85ca7688363b
jo <chjo0330@gmail.com>
2022-05-26 15:58:34 +0900

    .

--- SW Expert Academy/D1/간단한 N 의 약수.py
+++ SW Expert Academy/D1/간단한 N 의 약수.py
@@ -1,10 +1,20 @@
-n = int(input())
+#1
+# n = int(input())
 
-result = ''
+# result = ''
 
-for i in range(n):
-    if n % (i+1) == 0:
-        result += str(i+1)
-        result += ' '
+# for i in range(n):
+#     if n % (i+1) == 0:
+#         result += str(i+1)
+#         result += ' '
 
-print(result)
+# print(result)
+
+
+
+#2
+N = int(input())
+
+for index in range(1, N+1):
+    if N % index == 0:
+        print(index, end= ' ')
