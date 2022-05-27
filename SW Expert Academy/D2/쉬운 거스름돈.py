b08eca660e60a24a24de1783e25bffd449abe8a1
jo <chjo0330@gmail.com>
2022-05-27 18:38:54 +0900

    .

--- SW Expert Academy/D2/쉬운 거스름돈.py
+++ SW Expert Academy/D2/쉬운 거스름돈.py
@@ -1,66 +1,124 @@
 
+#1
+# T = int(input())
+
+# for i in range(1, T+1):
+#     N = int(input())
+
+#     omanwon_count = 0
+#     manwon_count = 0
+#     ochunwon_count = 0
+#     chunwon_count = 0
+#     obackwon_count = 0
+#     backwon_count = 0
+#     oshipwon_count = 0
+#     shipwon_count = 0
+
+#     if N < 50000:
+#         omanwon_count = 0
+#     else:
+#         omanwon_count = N // 50000
+#         N %= 50000
+    
+#     if N < 10000:
+#         manwon_count = 0
+#     else:
+#         manwon_count = N // 10000
+#         N %= 10000
+        
+#     if N < 5000:
+#                 ochunwon_count = 0
+#     else:
+#         ochunwon_count = N // 5000
+#         N %= 5000
+
+#     if N < 1000:
+#         chunwon_count = 0
+#     else:
+#         chunwon_count = N // 1000
+#         N %= 1000
+
+#     if N < 500:
+#         obackwon_count = 0
+#     else:
+#         obackwon_count = N // 500
+#         N %= 500
+
+#     if N < 100:
+#         backwon_count = 0
+#     else:
+#         backwon_count = N //100
+#         N %= 100
+
+#     if N < 50:
+#         oshipwon_count = 0
+#     else:
+#         oshipwon_count = N // 50
+#         N %= 50
+
+#     if N < 10:
+#         shipwon_count = 0
+#     else:
+#         shipwon_count = N // 10
+#         N %= 10
+#     print("#" + str(i))
+#     print('{} {} {} {} {} {} {} {}'.format(omanwon_count, manwon_count, ochunwon_count, chunwon_count, obackwon_count, backwon_count, oshipwon_count, shipwon_count))
+
+    
+#2
 T = int(input())
 
-for i in range(1, T+1):
+for testcase in range(1, T+1):
     N = int(input())
 
-    omanwon_count = 0
-    manwon_count = 0
-    ochunwon_count = 0
-    chunwon_count = 0
-    obackwon_count = 0
-    backwon_count = 0
-    oshipwon_count = 0
-    shipwon_count = 0
+    a = b = c = d = e = f = g = h = 0
 
     if N < 50000:
-        omanwon_count = 0
+        a = 0
     else:
-        omanwon_count = N // 50000
-        N %= 50000
-    
+        a = N // 50000
+        N = N % 50000
+
     if N < 10000:
-        manwon_count = 0
+        b = 0
     else:
-        manwon_count = N // 10000
-        N %= 10000
-        
+        b = N // 10000
+        N = N % 10000
+
     if N < 5000:
-                ochunwon_count = 0
+        c = 0
     else:
-        ochunwon_count = N // 5000
-        N %= 5000
+        c = N // 5000
+        N = N % 5000
 
     if N < 1000:
-        chunwon_count = 0
+        d = 0
     else:
-        chunwon_count = N // 1000
-        N %= 1000
+        d = N // 1000
+        N = N % 1000
 
     if N < 500:
-        obackwon_count = 0
+        e = 0
     else:
-        obackwon_count = N // 500
-        N %= 500
+        e = N // 500
+        N = N % 500
 
     if N < 100:
-        backwon_count = 0
+        f = 0
     else:
-        backwon_count = N //100
-        N %= 100
+        f = N // 100
+        N = N % 100
 
     if N < 50:
-        oshipwon_count = 0
+        g = 0
     else:
-        oshipwon_count = N // 50
-        N %= 50
+        g = N // 50
+        N = N % 50
 
     if N < 10:
-        shipwon_count = 0
+        h = 0
     else:
-        shipwon_count = N // 10
-        N %= 10
-    print("#" + str(i))
-    print('{} {} {} {} {} {} {} {}'.format(omanwon_count, manwon_count, ochunwon_count, chunwon_count, obackwon_count, backwon_count, oshipwon_count, shipwon_count))
-
-    
+        h = N // 10
+        N = N % 10
+    print("#" + str(testcase))
+    print("{} {} {} {} {} {} {} {}".format(a, b, c, d, e, f, g, h))
