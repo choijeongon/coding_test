b53fc2660ada8508ea5b05c1bf286be7d442fba9
jo <chjo0330@gmail.com>
2022-05-27 16:57:41 +0900

    파스칼의 삼각형 업데이트

--- SW Expert Academy/D2/파스칼의 삼각형.py
+++ SW Expert Academy/D2/파스칼의 삼각형.py
@@ -1,30 +1,52 @@
 
 
+#1
+# T = int(input())
+
+# result_map = {}
+
+# for i in range(0, T):
+#     N = int(input())
+
+#     arr = [[0] * N for _ in range(N)]
+
+#     for i in range(N):
+#         for j in range(i+1):
+#             if j==0 or j==i:
+#                 arr[i][j] = 1
+#             else:
+#                 arr[i][j] = arr[i-1][j] + arr[i-1][j-1]
+    
+#     result_map[i+1] = arr
+
+# for i in range(0, T):
+#     print("#" + str(i+1))
+#     for j in range(len(result_map[i+1])):
+#         for k in range(len(result_map[i+1][j])):
+#             if result_map[i+1][j][k] != 0:
+#                 print(result_map[i+1][j][k], end = " ")
+#         print()
+
+#2
 T = int(input())
 
-result_map = {}
-
-for i in range(0, T):
+for testcase in range(1, T+1):
     N = int(input())
 
-    arr = [[0] * N for _ in range(N)]
+    board = [[0] * N for _ in range(N)]
 
     for i in range(N):
         for j in range(i+1):
-            if j==0 or j==i:
-                arr[i][j] = 1
+            if j == 0 or j == i:
+                board[i][j] = 1
             else:
-                arr[i][j] = arr[i-1][j] + arr[i-1][j-1]
-    
-    result_map[i+1] = arr
+                board[i][j] = board[i-1][j] + board[i-1][j-1]\
 
-for i in range(0, T):
-    print("#" + str(i+1))
-    for j in range(len(result_map[i+1])):
-        for k in range(len(result_map[i+1][j])):
-            if result_map[i+1][j][k] != 0:
-                print(result_map[i+1][j][k], end = " ")
+    print("#" + str(testcase))
+    for index in range(len(board)):
+        result = board[index]
+        for value in result:
+            if value != 0:
+                print(value, end = ' ')
         print()
-
-
     
