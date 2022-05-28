24c41520c8600a13cc1ee711052c1a94b575ea65
jo <chjo0330@gmail.com>
2022-05-28 12:59:49 +0900

    .

--- My_library/에라토스테네스의 체(소수).py
+++ My_library/에라토스테네스의 체(소수).py
@@ -1,14 +1,13 @@
-import collections
-max = 10 ** 3
-sieve = collections.defaultdict(lambda: True)
-print(sieve)
-do = int(max ** (1/2))
 
-for i in range(2, do+1):
-    if sieve[i]:
-        for j in range(i+i, max+1, i):
-            sieve[j] = False
+def prime_list(n):
+    #n개 요소에 True로 설정(소수로 간주)
+    sieve = [True] * n
 
-for n in range(2, max+1):
-    if sieve[n]:
-        print(n, end=' ')
+    # n의 최대 약수가 sqrt(n) 이하이므로 i = sqrt(n)까지 검사
+    m = int(n ** 0.5)
+    for i in range(2, m+1):
+        if sieve[i] == True: #i가 소수라면
+            for j in range(i+i, n, i): #i이후 i의 배수들을 False 판정
+                sieve[j] = False
+    
+    return [i for i in range(2, n) if sieve[i] == True]
