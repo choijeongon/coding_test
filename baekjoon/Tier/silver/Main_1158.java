39c04a452ae179a25e1df2b5a934186ca7a60487
최정온 <chjo0330@gmail.com>
2022-08-08 22:10:03 +0900

    백준 1158 문제 풀이

--- baekjoon/Tier/silver/Main_1158.java
+++ baekjoon/Tier/silver/Main_1158.java
@@ -1,20 +1,48 @@
 package silver;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main_1158 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> resultList = new ArrayList<>();
+		Queue<Integer> queue = new LinkedList<Integer>();
 		
 		int N = sc.nextInt();
 		int K = sc.nextInt();
 		
-		int[] list = new int[N];
+		for (int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
 		
-		for (int i = 1; i <= list.length; i++) {
-			list[(i-1)] = i;
+		// count == 3이 될때마다 pop
+		int count = 0;
+		
+		while(!queue.isEmpty()) {
+			for (int i = 0; i < N; i++) {
+				count++;
+				
+				if((count % K) == 0) {
+					resultList.add(queue.poll());
+//				System.out.println(queue.poll());
+				} else {
+					queue.add(queue.poll());
+				}
+			}
 		}
 		
 		
+		System.out.print("<");
+		for (int i = 0; i < resultList.size(); i++) {
+			if(i == resultList.size()-1) {
+				System.out.print(resultList.get(i));
+			} else {
+				System.out.print(resultList.get(i)+", ");
+			}
+		}
+		System.out.print(">");
 	}
 }
