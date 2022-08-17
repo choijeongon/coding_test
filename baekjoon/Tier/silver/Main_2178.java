d2b19552a98dbdd3bfa3aa42dd0617baa48d445e
최정온 <chjo0330@gmail.com>
2022-08-17 10:22:59 +0900

    백준 2178번 문제 해결

--- baekjoon/Tier/silver/Main_2178.java
+++ baekjoon/Tier/silver/Main_2178.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_2178 {
@@ -12,9 +14,7 @@ public class Main_2178 {
 	static int M;
 	
 	static int[][] board;
-	static int[][] copyBoard;
-	
-	static int count;
+//	static int[][] copyBoard;
 	
 	static final int[] dx = {-1, 0, +1, 0}; //up, right, down, left
 	static final int[] dy = {0, +1, 0, -1};
@@ -28,15 +28,13 @@ public class Main_2178 {
 		M = Integer.parseInt(st.nextToken());
 		
 		board = new int[N][M];
-		copyBoard = new int[N][M];
-		
-		count = Integer.MAX_VALUE;
+//		copyBoard = new int[N][M];
 		
 		for (int i = 0; i < N; i++) {
 			String line = br.readLine();
 			for (int j = 0; j < M; j++) {
 				board[i][j] = line.charAt(j) - '0';
-				copyBoard[i][j] = board[i][j];
+//				copyBoard[i][j] = board[i][j];
 			}
 		}
 		//Input End
@@ -45,55 +43,70 @@ public class Main_2178 {
 //			System.out.println(Arrays.toString(line));
 //		}
 		
-		dfs(0,0,1);
+		bfs();
 		
 		//output
-		System.out.println(count);
+		System.out.println(board[N-1][M-1]);
 	}
 	
 	static void print() {
-		for(int[] list :  copyBoard) {
+		for(int[] list :  board) {
 			System.out.println(Arrays.toString(list));
 		}
 		System.out.println("-------------------------------");
 	}
 	
-	static void dfs(int x, int y, int cnt) {
-		//방문 표시
-		copyBoard[x][y] = 0;
+	
+	static void bfs() {
+		 Queue<Position> queue = new LinkedList<Position>();
+		 queue.offer(new Position(0,0,1));
+		 
+		 while(!queue.isEmpty()) {
+			 Position currentPosition = queue.poll();
+			 	int x = currentPosition.x;
+			 	int y = currentPosition.y;
+			 	int count = currentPosition.count;
+			 
+			 
+				for (int direct = 0; direct < 4; direct++) {
+					int nextX = x + dx[direct];
+					int nextY = y + dy[direct];
+					
+					//범위 벗어나는지 체크
+					if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) {
+						continue;
+					}
+					
+					//이미 방문한 곳으로 돌아갈 수 없음
+					if(board[nextX][nextY] > 1) {
+						continue;
+					}
+					
+					//0인곳에는 못감
+					if(board[nextX][nextY] == 0) {
+						continue;
+					}
+					
+//					System.out.println("cnt= " + currentPosition.count);
+//					print();
+					
+					queue.offer(new Position(nextX, nextY, (count+1)));
+					//방문 표시
+					board[nextX][nextY] = count+1;
+				}
+		 }
+		 
+	}
+	
+	static class Position{
+		int x;
+		int y;
+		int count;
 		
-		//기저 조건
-		if(x == (N-1) && y == (M-1)) {
-			count = Math.min(count, cnt);
-			return;
-		}
-		
-		//델타 탐색
-		for (int direct = 0; direct < 4; direct++) {
-			int nextX = x + dx[direct];
-			int nextY = y + dy[direct];
-			
-			//범위 벗어나는지 체크
-			if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) {
-				continue;
-			}
-			
-			//이미 방문한 곳으로 돌아갈 수 없음
-			if(copyBoard[nextX][nextY] == 0) {
-				continue;
-			}
-			
-			//0인곳에는 못감
-			if(board[x][y] == 0) {
-				continue;
-			}
-			System.out.println("cnt= " + cnt);
-			print();
-			
-			dfs(nextX, nextY, cnt+1);
-			//여기를 방문 안했을때도 돌려보기
-			copyBoard[x][y] = 1;
-//			dfs(nextX, nextY, cnt);
+		public Position(int x, int y, int count) {
+			this.x = x;
+			this.y = y;
+			this.count = count;
 		}
 	}
 }
--- baekjoon/Tier/test/Main.java
+++ baekjoon/Tier/test/Main.java
@@ -1,71 +1,49 @@
 package test;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-/*
-N개의 화학 물질 c1...cn
-ci마다 최저 보관 온도 xi와 최고 보관 온도 yi
-xi <= ci <= yi
-
- 여러 대의 냉장고 가능하면 적은 수의 냉장고
- 1 <= N <= 100
- 
- -270 ~ 10000
-*/
-
 public class Main {
+	static int N;
+	static long ans;
+	static Ingredient[] ingredients;
+	
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int N = sc.nextInt();
-
-		ChemicalSubstance[] chemicalSubstanceList = new ChemicalSubstance[N];
-
-		for (int i = 0; i < N; i++) {
-			chemicalSubstanceList[i] = new ChemicalSubstance(sc.nextInt(), sc.nextInt());
+		 N = sc.nextInt();
+		 
+		 ingredients = new Ingredient[N];
+		 
+		 for (int i = 0; i < N; i++) {
+			ingredients[i] = new Ingredient(sc.nextInt(), sc.nextInt()); // 신맛, 쓴맛
 		}
-		// Input End
-
-		// 정렬
-		Arrays.sort(chemicalSubstanceList);
-
-//		System.out.println(Arrays.toString(chemicalSubstanceList));
-		int count = 1;
-		
-		// 첫 번째 최대 온도
-		int refrigeratorTemperature = chemicalSubstanceList[0].maxTemperature;
-		
-		for (int i = 1; i < chemicalSubstanceList.length; i++) {
-			ChemicalSubstance currentChemicalSubstance = chemicalSubstanceList[i];
-			
-			if(refrigeratorTemperature < currentChemicalSubstance.minTemperature) {
-				count++;
-				refrigeratorTemperature = currentChemicalSubstance.maxTemperature;
+		 
+		 ans = Long.MAX_VALUE;
+		 subset();
+		 
+		 System.out.println(ans);
+	}
+	
+	private static void subset() {
+//		for (int select = 0; select < Math.pow(2, N); select++) { // 2^N
+		for (int select = 0; select < (1<<N); select++) { 
+			for (int idx = 0; idx < N; idx++) {
+				if(((1<<idx)&select) > 0) { //해당 자리의 bit가 1이면 부분 집합에 idx번 재료가 포함되었구나!
+					
+				}
 			}
+			// idx를 N만큼 돌려서 재료 포함여부로 신만 쓴맛 계산 했으면
 		}
-		
-		System.out.println(count);
 	}
 
-	static class ChemicalSubstance implements Comparable<ChemicalSubstance> {
-		int minTemperature;
-		int maxTemperature;
-
-		public ChemicalSubstance(int minTemperature, int maxTemperature) {
-			this.minTemperature = minTemperature;
-			this.maxTemperature = maxTemperature;
-		}
-
-		@Override
-		public String toString() {
-			return "ChemicalSubstance [minTemperature=" + minTemperature + ", maxTemperature=" + maxTemperature + "]\n";
-		}
-
-		@Override
-		public int compareTo(ChemicalSubstance o) { //최저 온도 기준 오름차순, 최저 온도가 같다면 최고 온도 기준 오름차순
-			return this.maxTemperature != o.maxTemperature ? this.maxTemperature - o.maxTemperature
-					: this.minTemperature - o.minTemperature;
+	static class Ingredient{
+		int bitter;
+		int sour;
+		
+		public Ingredient(int s, int b) {
+			sour = s;
+			bitter = b;
 		}
 	}
 }
