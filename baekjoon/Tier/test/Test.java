1248dbdb9c79caad7d23667d96b165cf96a7a576
최정온 <chjo0330@gmail.com>
2022-08-22 20:49:17 +0900

    백준 11286 문제 해결

--- baekjoon/Tier/test/Test.java
+++ baekjoon/Tier/test/Test.java
@@ -1,68 +1,42 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Test {
-	static int n; //입력 인원수
-	static int s, e, m;
-	static ArrayList<Integer>[] list;
-	static int res = -1;
-	static boolean[] v;
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		ABS[] list = new ABS[3];
 		
-		n = sc.nextInt();
-		s = sc.nextInt();
-		e = sc.nextInt();
-		m = sc.nextInt();
+		list[0] = new ABS(10);
+		list[1] = new ABS(5);
+		list[2] = new ABS(13);
 		
-		list = new ArrayList[n+1];
-		v = new boolean[n+1];
 		
-		for (int i = 0; i <= n; i++) {
-			list[i] = new ArrayList();
-		}
 		
-		for(int i =0; i <m; i++) {
-			int from = sc.nextInt();
-			int to = sc.nextInt();
-			list[from].add(to);
-			list[to].add(from);
-		} // 관계 입력
-		bfs();
-		
-		System.out.println(res);
+		System.out.println(Arrays.toString(list));
 	}
 	
-	static void bfs() {
-		Queue<Integer> q = new LinkedList();
-		q.offer(s);
-		v[s] = true;
-		int depth = 0;
-		while(!q.isEmpty()) {
-			//depth 관리
-			int size = q.size();
-			for(int i=0; i< size; i++) {
-				int cur = q.poll();
-				if(cur == e) {
-					res = depth;
-				}
-				for(int idx : list[cur]) {
-					//방문했으면 통과
-					if(v[idx]) {
-						continue;
-					}
-					q.offer(idx);
-					v[idx] = true;
-				}
-			}
-			depth++;
-			
+	static class ABS implements Comparator<ABS>{
+		int value;
+
+		public ABS(int value) {
+			this.value = value;
 		}
 		
+		@Override
+		public String toString() {
+			return Integer.toString(value);
+		}
+
+		@Override
+		public int compare(ABS o1, ABS o2) {
+			return o1.value - o2.value;
+		}
+
+//		@Override
+//		public int compareTo(ABS o) {
+//			return this.value - o.value;
+//		}
 	}
 }
