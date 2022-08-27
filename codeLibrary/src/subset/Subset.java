6265e0e0c41062d78f3abd57c2cca10e9b522ebb
최정온 <chjo0330@gmail.com>
2022-08-27 18:11:01 +0900

    부분집합 코드 추가

---
+++ codeLibrary/src/subset/Subset.java
@@ -0,0 +1,43 @@
+package subset;
+
+import java.util.Scanner;
+
+public class Subset {
+	
+	static int N;
+	
+	static int[] inputList;
+	static boolean[] isSelected;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		
+		inputList = new int[N];
+		isSelected = new boolean[N];
+		
+		for (int i = 0; i < N; i++) {
+			inputList[i] = sc.nextInt();
+		}
+		//Input End
+		
+		subset(0);
+	}
+	
+	static void subset(int index) {
+		if(index == N) {
+			for (int i = 0; i < N; i++) {
+				System.out.print(isSelected[i] ? inputList[i] : "X");
+				System.out.print("\t");
+			}
+			System.out.println();
+			return;
+		}
+		
+		isSelected[index] = true;
+		subset(index+1);
+		isSelected[index] = false;
+		subset(index+1);
+	}
+}
