c2fa127c303aac88c35929aea76f92e362216500
최정온 <chjo0330@gmail.com>
2023-01-18 23:47:18 +0900

    2954

---
+++ baekjoon/Tier/bronze/Main_2954_2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String originalStr = "";
+        // 빼야하는 카운트
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if(count > 0){
+                count--;
+                continue;
+            }
+
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+                originalStr += str.charAt(i);
+                count = 2;
+            } else {
+                originalStr += str.charAt(i);
+            }
+//            System.out.print(str.charAt(i));
+        }
+        System.out.println(originalStr);
+    }
+}
